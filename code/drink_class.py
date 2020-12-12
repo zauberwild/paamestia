84b7fe7f7a26096b5938b3caa1d5754a3af524e2
zauberwild <zauberwild@users.noreply.github.com>
2020-12-12 21:40:09 +0100

    Update drink_class.py

--- code/drink_class.py
+++ code/drink_class.py
@@ -3,3 +3,25 @@ drink_class.py
 
 contains the drink-class
 """
+
+class drink:
+	"""
+	this class handles the spots, remembers the plugged drinks
+	and controls the output
+	"""
+
+	def __init__(self,spot):
+		self.spot = spot
+		self.drink
+
+	def assignDrink(self,drink):
+		""" set a new drink
+		- drink: the new drink
+		"""
+		pass
+
+	def fill(self,vol):
+		"""	start to fill the glass
+		- vol: the needed volume in ml
+		"""
+		pass

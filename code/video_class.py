2367d0cfc2b2ddc27499347db40ca45a10c16aef
zauberwild <zauberwild@users.noreply.github.com>
2020-12-12 19:48:29 +0100

    Update video_class.py

--- code/video_class.py
+++ code/video_class.py
@@ -3,3 +3,19 @@ video_class.py
 
 contains class for videos
 """
+
+class video:
+	"""
+	this class handles the sprites/spritesheets folders and makes videos out of these.
+	It is also possible to give a list of folder-paths, in this case the video will be
+	chosen randomly.
+	"""
+
+	def __init__(self):
+		play = False
+
+	def play(self, forwards=True):
+		"""	start video 
+		!! will start video from beginning. please use pause() to unpause
+		- forwards=True: set False, if you want play backwards
+		"""

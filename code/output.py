3e1d9115e85a1c727a942b7a1d091f48455b92d9
zauberwild <zauberwild@users.noreply.github.com>
2020-12-13 16:09:57 +0100

    Update output.py

--- code/output.py
+++ code/output.py
@@ -1,9 +1,16 @@
 """
 output.py
 
-this script handles the output to the relays / valves and pump
+PLACEHOLDER
 """
 
-"""
-ISSUE EVT. NOT NEEDED
-"""
+class LED_PH:
+	def __init__(self, pin):
+		self.pin = pin
+		self.val = False
+
+	def on(self):
+		pass
+		
+	def off(self):
+		pass

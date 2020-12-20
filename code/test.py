bc583e8f1f41b9b9bf4275ad821ffbf649b8fabe
zauberwild <zauberwild@users.noreply.github.com>
2020-12-20 17:13:09 +0100

    Update test.py

--- code/test.py
+++ code/test.py
@@ -4,4 +4,8 @@ PLACEHOLDER for testing
 
 import video_class
 
-test_vid = video_class.video()
+test_vid = video_class.video("/lib/test/","frames-",5,".png")
+
+#test_vid.testFeedback()
+
+print(video_class.getFrameIndex(5,2))

7dbcfecc6bce89d0d01210538300ea7cede2e576
zauberwild <zauberwild@users.noreply.github.com>
2020-12-20 17:59:28 +0100

    Update test.py

--- code/test.py
+++ code/test.py
@@ -5,6 +5,6 @@ PLACEHOLDER for testing
 import video_class, pygame
 pygame.init()
 
-test_vid = video_class.video("/lib/test/","",4,".png")
+test_vid = video_class.video("/lib/src/intro/","",4,".png")
 
 test_vid.testFeedback()

c9d8fd0b873cc4ea2458cc11ccefb085fc6abeae
zauberwild <zauberwild@users.noreply.github.com>
2020-12-21 12:59:46 +0100

    merged content of recipelib.py to drinkslib.py

--- code/drinkslib.py
+++ code/drinkslib.py
@@ -1,8 +1,32 @@
 """
 drink_class.py
 
-contains the drink-class
+contains the recipe-class and drin-class
 """
+
+class recipe:
+	"""
+	stores and handles the available recipes
+	"""
+
+	def __init__(self, path):
+		self.path = path
+		self.description
+		self.drinks = []
+		self.vol = []
+		self.loadRecipe()
+
+	def loadRecipe(self, new_path = ""):
+		""" load recipe
+		- new_path: use to set a new recipe up
+		"""
+		if new_path == "":
+			new_path = self.path
+	
+	def mixRecipe(self):
+		"""
+		ISSUE [i2] needed? 
+		"""
 
 class drink:
 	"""

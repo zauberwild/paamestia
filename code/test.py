401c5e7f23670332c43e01f908f388d13a33ecd8
zauberwild <zauberwild@users.noreply.github.com>
2020-12-21 16:10:57 +0100

    Update test.py

--- code/test.py
+++ code/test.py
@@ -20,7 +20,7 @@ test_vid = medialib.video("/src/test/")
 
 video_list = []
 for i in range(15):
-	video_list.append(medialib.video("/src/test/"))
+	video_list.append(medialib.video("/src/test/",i))
 
 print("Video loaded!")
 

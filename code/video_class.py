f1a40df3d95bc114a20cca82d7986c4a60a9c49f
zauberwild <zauberwild@users.noreply.github.com>
2020-12-21 11:55:30 +0100

    Update video_class.py

--- code/video_class.py
+++ code/video_class.py
@@ -4,7 +4,7 @@ video_class.py
 contains class for videos
 """
 
-import os, pathlib, pygame
+import os, pathlib, pygame		#ISSUE [i1] how to handle pygame/drawing the video? (REF at Varroa Invaders)	
 gen_path = str(pathlib.Path(__file__).parent.absolute())
 
 class video:
@@ -12,29 +12,26 @@ class video:
 	this class handles the sprites/spritesheets folders and makes videos out of these.
 	It is also possible to give a list of folder-paths, in this case the video will be
 	chosen randomly.
+	ISSUE Won't be included, if not specifcally needed
 	"""
 
-	def __init__(self, video_path,surfix,digits,prefix):
+	def __init__(self, video_path):
 		""" vieo class. uses sprites do display a video
 		- video_path: path to folder
-		- surfix: file-surfix (e.g. frames-)
-		- prefix: file-prefix (e.g. .png)
 		"""
 		self.path = gen_path + video_path
-		self.surfix = surfix
-		self.digits = digits
-		self.prefix = prefix
 		self.img = []
+		self.audio = ""
 		
-		for i, filename in enumerate(os.listdir(self.path)):
-			index = getFrameIndex(self.digits,i+1)
-			print(self.path + self.surfix + index + self.prefix)
-			self.img.append(pygame.image.load(self.path + self.surfix + index + self.prefix))
+		for filename in os.listdir(self.path):
+			if ".wav" in filename:					# filters the audio file out
+				self.audio = self.path + filename
+			else:
+				#print(self.path + filename)											#DEL
+				self.img.append(pygame.image.load(self.path + filename))
 		
+		self.frames = len(self.img)
 		self.play = False
-	
-	def testFeedback(self):
-		pass
 
 	def start(self, forwards=True):
 		"""	start video 
@@ -44,6 +41,7 @@ class video:
 
 	def pause(self):
 		""" pause / unpause the video
+		ISSUE is it neccesarry?
 		"""
 		pass
 
@@ -51,22 +49,6 @@ class video:
 		""" draws the video
 		"""
 		pass
-
-def getFrameIndex(digits, index):
-	""" return string of index with trailing zeros
-	- digits: number of digits
-	- index: number to return
-	"""
-	index_str = str(index)
-	len_of_index = len(str(index))
-
-	out = ""
-	for i in range(digits - len_of_index):
-		out += "0"
-	
-	out += str(index)
-
-	return out
 	
 
 

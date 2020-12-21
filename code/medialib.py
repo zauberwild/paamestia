20780639abad772272e33e799b923ad50d3deea8
zauberwild <zauberwild@users.noreply.github.com>
2020-12-21 16:06:26 +0100

    Update medialib.py

--- code/medialib.py
+++ code/medialib.py
@@ -1,18 +1,13 @@
 """
-video_class.py
-
 contains class for videos
 """
 
-import os, pathlib, pygame		#ISSUE [i1] how to handle pygame/drawing the video? (REF at Varroa Invaders)	
+import os, pathlib, pygame	
 gen_path = str(pathlib.Path(__file__).parent.absolute())
 
 class video:
 	"""
 	this class handles the sprites/spritesheets folders and makes videos out of these.
-	It is also possible to give a list of folder-paths, in this case the video will be
-	chosen randomly.
-	ISSUE Won't be included, if not specifcally needed
 	"""
 
 	def __init__(self, video_path):
@@ -21,34 +16,67 @@ class video:
 		"""
 		self.path = gen_path + video_path
 		self.img = []
-		self.audio = ""
+		self.audio_path = ""
 		
 		for filename in os.listdir(self.path):
 			if ".wav" in filename:					# filters the audio file out
-				self.audio = self.path + filename
+				self.audio_path = self.path + filename
 			else:
 				#print(self.path + filename)											#DEL
 				self.img.append(pygame.image.load(self.path + filename))
 		
-		self.frames = len(self.img)
-		self.play = False
+		self.audio = pygame.mixer.Sound(self.audio_path)
 
-	def start(self, forwards=True):
+		self.frames = len(self.img)
+
+		self.play = False
+		self.cur_frame = 0
+		self.forwards = True
+		self.repeat = False
+
+	def start(self, forwards=True, repeat=False):
 		"""	start video 
 		- forwards=True: set False, if you want play it backwards
 		"""
-		pass
+		self.play = True
+		self.forwards = forwards
+		self.repeat = repeat
+		if forwards:
+			self.cur_frame = 0
+		else:
+			self.cur_frame = self.frames - 1
+
+		pygame.mixer.Sound.play(self.audio)
+
+		print("now playing, forwards=" + str(self.forwards) + ", repeat=" + str(self.repeat))
 
 	def pause(self):
 		""" pause / unpause the video
-		ISSUE is it neccesarry?
 		"""
-		pass
+		self.play != self.play
 
-	def draw(self):
+	def draw(self, local_screen):
 		""" draws the video
 		"""
-		pass
+		if self.play:
+			local_screen.blit(self.img[self.cur_frame], (0, 0))
+			
+			if self.forwards:
+				self.cur_frame += 1
+				if self.cur_frame >= self.frames:
+					if self.repeat:
+						self.cur_frame = 0
+					else:
+						self.play = False
+						print("stopped playing")
+			else:
+				self.cur_frame -= 1
+				if self.cur_frame < 0:
+					if self.repeat:
+						self.cur_frame = self.frames - 1
+					else:
+						self.play = False
+						print("stopped playing")			
 	
 
 

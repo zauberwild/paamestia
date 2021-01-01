3a14e93fb3a47de1df3fe5d4f863819d6c3d2d7b
zauberwild <zauberwild@users.noreply.github.com>
2021-01-01 21:06:22 +0100

    Update test.py

--- code/test.py
+++ code/test.py
@@ -15,12 +15,9 @@ clock = pygame.time.Clock()
 screen.fill((0,0,255))
 pygame.display.flip()
 
-test_vid = medialib.video("/src/test/")
+test_vid = medialib.Animation("/src/test/")
 
-for i in test_vid.img_path:
-	print(i)
-
-prog_active = False
+prog_active = True
 while prog_active:
 	# input
 	for event in pygame.event.get():
@@ -29,14 +26,23 @@ while prog_active:
 			prog_active = False
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_1:
-				test_vid.start()
+				print("### playing")
+				test_vid.start(audio=False)
 			if event.key == pygame.K_2:
-				test_vid.start(False)
-			if event.key == pygame.K_7:
-				test_vid.img.clear()
-				for i in range(15):
-					video_list[i].img.clear()
-				print("cleared!")
+				test_vid.start(forwards=False, audio=False)
+				print("### playing reverse")
+			if event.key == pygame.K_3:
+				test_vid.load()
+				print("### Loading done")
+			if event.key == pygame.K_4:
+				test_vid.unload()
+				print("### cleared!")
+			if event.key == pygame.K_5:
+				test_vid.pause()
+				print("### pause/play")
+			if event.key == pygame.K_6:
+				test_vid.stop()
+				print("### stopped!")
 	
 	screen.fill((127,127,127))
 

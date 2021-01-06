7ef514177817e9ee57e173705c957c74a324b63d
zauberwild <zauberwild@users.noreply.github.com>
2021-01-06 14:35:19 +0100

    Update cv2_test.py

--- code/cv2_test.py
+++ code/cv2_test.py
@@ -20,6 +20,34 @@ x1, x2 = x_offset, x_offset + img.shape[1]
 alpha_s = img[:, :, 3] / 255.0
 alpha_l = 1.0 - alpha_s
 
+import cv2
+
+def merge_image(back, front, x,y):
+    # convert to rgba
+    if back.shape[2] == 3:
+        back = cv2.cvtColor(back, cv2.COLOR_BGR2BGRA)
+    if front.shape[2] == 3:
+        front = cv2.cvtColor(front, cv2.COLOR_BGR2BGRA)
+
+    # crop the overlay from both images
+    bh,bw = back.shape[:2]
+    fh,fw = front.shape[:2]
+    x1, x2 = max(x, 0), min(x+fw, bw)
+    y1, y2 = max(y, 0), min(y+fh, bh)
+    front_cropped = front[y1-y:y2-y, x1-x:x2-x]
+    back_cropped = back[y1:y2, x1:x2]
+
+    alpha_front = front_cropped[:,:,3:4] / 255
+    alpha_back = back_cropped[:,:,3:4] / 255
+    
+    # replace an area in result with overlay
+    result = back.copy()
+    print(f'af: {alpha_front.shape}\nab: {alpha_back.shape}\nfront_cropped: {front_cropped.shape}\nback_cropped: {back_cropped.shape}')
+    result[y1:y2, x1:x2, :3] = alpha_front * front_cropped[:,:,:3] + alpha_back * back_cropped[:,:,:3]
+    result[y1:y2, x1:x2, 3:4] = (alpha_front + alpha_back) / (1 + alpha_front*alpha_back) * 255
+
+    return result
+
 while(cap.isOpened()):
 	
 	time_elapsed = time.time() - prev

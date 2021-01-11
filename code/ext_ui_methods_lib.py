ad99049947c03a38bd57c31b26a14799c7327652
zauberwild <zauberwild@users.noreply.github.com>
2021-01-11 18:50:15 +0100

    Create ext_ui_methods_lib.py
    created new file for ui things

---
+++ code/ext_ui_methods_lib.py
@@ -0,0 +1,42 @@
+"""
+this file include all methods for the user interface
+"""
+
+import pygame				# ISSUE [i7] drawing single sprites: in method or as class in medialib ?
+from globals import *
+import media_lib
+import drinks_lib
+import io_lib
+
+""" ### ### INTRO / MAIN MENU ### ### """
+def intro():
+	pass
+
+def main_menu():
+	pass
+
+""" ### ### FREE MIXING ### ### """
+def free_transition():
+	pass
+
+def free_choose():
+	pass
+
+def free_output():
+	pass
+
+
+""" ### ### RECIPE ### ### """
+def recipe_transition():
+	pass
+
+def recipe_choose():
+	pass
+
+def recipe_output():
+	pass
+
+
+""" ### ### SETTINGS ### ### """
+def settings():
+	pass

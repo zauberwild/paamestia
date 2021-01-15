f48c15be7933c6228fe7b2439a175f6cd4eab628
zauberwild <zauberwild@users.noreply.github.com>
2021-01-15 14:50:24 +0100

    Update globals.py
    moved prog_active to globals

--- code/globals.py
+++ code/globals.py
@@ -7,11 +7,12 @@ gen_path = str(Path(__file__).parent.absolute())		# get the complete path of the
 from os import path											# used to get the complete path of the working directory
 os_is_linux = not path.isfile(gen_path + "/src/.windows")		# look for a ".windows" file, which only exists on my Windows-PC
 
+prog_active = True		# set to False to end program
+
 FPS = 24				# frames per second
 W, H = 800, 600			# width and height of the window
 
 prog_pos = 'i'			# saves current position in program flow
-
 
 show_debug = True		# show debugging information (fps,...)
 debug_text = []			# debug list with all parameters to show. append parameters to this list every loop

f673ccfc298d7fcae816a9f83544dfe5b311315d
zauberwild <zauberwild@users.noreply.github.com>
2021-01-15 14:53:08 +0100

    Update paamestia.py
    - changed global import from * to gl
    - small changs
    - removed keyboard input -> now in io_lib

--- code/paamestia.py
+++ code/paamestia.py
@@ -4,8 +4,9 @@ paamestia
 """
 
 # imports
-from globals import *
+import globals as gl
 import ext_ui_methods_lib as ui
+import io_lib
 
 # pygame stuff
 import pygame
@@ -14,76 +15,70 @@ pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
 pygame.mixer.init()
 pygame.freetype.init()
-debug_font = pygame.freetype.Font(gen_path + "/src/fonts/CamingoCode-Regular.ttf", 24)
+debug_font = pygame.freetype.Font(gl.gen_path + "/src/fonts/CamingoCode-Regular.ttf", 24)
 
 # DEL fill screen, can be deleted, when project done
-screen.fill((0,0,127))
+gl.screen.fill((0,0,127))
 pygame.display.flip()
 
 # objects creation
 
 
 """ ### ### main loop ### ### """
-prog_active = True
-while prog_active:
+while gl.prog_active:
 	# keyboard input
-	for event in pygame.event.get():
-		# closing window / exit program
-		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-			prog_active = False
-		if event.type == pygame.KEYDOWN:
-			# debug information
-			if event.key == pygame.K_d:
-				show_debug = not show_debug
+	io_lib.keyboard_input()
 
 	# run external methods according to prog_pos
-	if prog_pos == 'i':					# intro
+	if gl.prog_pos == 'i':					# intro
 		ui.intro()
 
-	elif prog_pos == 'm':				# main menu
+	elif gl.prog_pos == 'm':				# main menu
 		ui.main_menu()
 
-	elif prog_pos[0] == 'f':			# free mixing
-		if prog_pos[1] == 't':				# transition
+	elif gl.prog_pos[0] == 'f':			# free mixing
+		if gl.prog_pos[1] == 't':				# transition
 			ui.free_transition()
-		elif prog_pos[1] == 'c':			# mix cocktail
+		elif gl.prog_pos[1] == 'c':			# mix cocktail
 			ui.free_choose()
-		elif prog_pos[1] == 'o':			# output
+		elif gl.prog_pos[1] == 'o':			# output
 			ui.free_output()
 
-	elif prog_pos[0] == 'r':			# recipe
-		if prog_pos[1] == 't':				# transition
+	elif gl.prog_pos[0] == 'r':			# recipe
+		if gl.prog_pos[1] == 't':				# transition
 			ui.recipe_transition()
-		elif prog_pos[1] == 'c':			# choose recipe / cocktail
+		elif gl.prog_pos[1] == 'c':			# choose recipe / cocktail
 			ui.recipe_choose()
-		elif prog_pos[1] == 'o':			# output
+		elif gl.prog_pos[1] == 'o':			# output
 			ui.recipe_output()
 
-	elif prog_pos[0] == 's':			# settings
+	elif gl.prog_pos[0] == 's':			# settings
 		ui.settings()
 
 	else:
-		text = ["ERROR", "invalid prog_pos: " + str(prog_pos), "resetting to 'm'"]
-		screen.fill((0,0,0))
+		text = ["ERROR", "invalid prog_pos: " + str(gl.prog_pos), "resetting to 'm'"]
+		gl.screen.fill((0,0,0))
 		h = 0
 		for t in text:
 			print(t)
-			debug_font.render_to(screen, (0,h), t, (255,0,0))
+			debug_font.render_to(gl.screen, (0,h), t, (255,0,0))
 			h += 32
 		prog_pos = 'm'
 
 
 	# debug
-	if show_debug:
-		fps = str(clock.get_fps())
-		debug_main_loop = ["FPS: " + fps[0:6], "prog_pos: " + prog_pos]
+	if gl.show_debug:
+		fps = str(gl.clock.get_fps())
+		debug_main_loop = ["FPS: " + fps[0:6], "prog_pos: " + gl.prog_pos]
 		h = 3
-		for t in debug_main_loop + debug_text:
+		for t in debug_main_loop + gl.debug_text:
 			textsur, rect = debug_font.render(t, (0, 255, 0))
-			pygame.draw.rect(screen, (0,0,0), (0,H - rect.height - h,rect.width,rect.height))
-			screen.blit(textsur, (0,H - rect.height - h))
+			pygame.draw.rect(gl.screen, (0,0,0), (0,gl.H - rect.height - h,rect.width,rect.height))
+			gl.screen.blit(textsur, (0,gl.H - rect.height - h))
 			h += rect.height + 3
-		debug_text.clear()
+		gl.debug_text.clear()
 
 	pygame.display.flip()	# refresh window and show content
-	clock.tick(FPS)			# limit fps
+	gl.clock.tick(gl.FPS)			# limit fps
+
+pygame.quit()

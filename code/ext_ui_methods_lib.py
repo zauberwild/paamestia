98942025e7907f2cb9ac337db243eb9f434e3b83
zauberwild <zauberwild@users.noreply.github.com>
2021-01-15 14:49:32 +0100

    Update ext_ui_methods_lib.py
    - changed globals import fom * to gl
    - small changes

--- code/ext_ui_methods_lib.py
+++ code/ext_ui_methods_lib.py
@@ -3,14 +3,26 @@ this file include all methods for the user interface
 """
 
 import pygame				# ISSUE [i7] drawing single sprites: in method or as class in medialib ?
-from globals import *
+import globals as gl
 import media_lib
 import drinks_lib
-import io_lib
+import io_lib as io
 
 """ ### ### INTRO / MAIN MENU ### ### """
 def intro():
-	screen.fill((127,127,127))
+	gl.screen.fill((127,127,127))
+	if io.readInput(io.UP):
+		io.writeOutput(io.VALVES[1], not io.valves_state[1])
+	if io.readInput(io.DOWN):
+		io.writeOutput(io.VALVES[2], not io.valves_state[2])
+	if io.readInput(io.LEFT):
+		io.writeOutput(io.VALVES[3], not io.valves_state[3])
+	if io.readInput(io.RIGHT):
+		io.writeOutput(io.VALVES[4], not io.valves_state[4])
+	if io.readInput(io.NEXT):
+		io.writeOutput(io.VALVES[0], 1)
+	if io.readInput(io.BACK):
+		io.writeOutput(io.VALVES[0], 0)
 
 def main_menu():
 	pass

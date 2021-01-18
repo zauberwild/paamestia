f4070448c44a4658799528435a0238a0c02f738f
zauberwild <zauberwild@users.noreply.github.com>
2021-01-18 18:02:34 +0100

    small changes
    - moved imports down
    - added shutdown
    - small changes

--- code/paamestia.py
+++ code/paamestia.py
@@ -1,12 +1,8 @@
 """
 paamestia
 
+main script
 """
-
-# imports
-import globals as gl
-import ext_ui_methods_lib as ui
-import io_lib
 
 # pygame stuff
 import pygame
@@ -15,25 +11,28 @@ pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
 pygame.mixer.init()
 pygame.freetype.init()
+
+# imports
+import globals as gl
+import ext_ui_methods_lib as ui
+
+# variables
 debug_font = pygame.freetype.Font(gl.gen_path + "/src/fonts/CamingoCode-Regular.ttf", 24)
 
 # DEL fill screen, can be deleted, when project done
 gl.screen.fill((0,0,127))
 pygame.display.flip()
 
-# objects creation
-
-
 """ ### ### main loop ### ### """
 while gl.prog_active:
-	# keyboard input
-	io_lib.keyboard_input()
+
+	ui.loop()
 
 	# run external methods according to prog_pos
 	if gl.prog_pos == 'i':					# intro
 		ui.intro()
 
-	elif gl.prog_pos == 'm':				# main menu
+	elif gl.prog_pos == 'm':			# main menu
 		ui.main_menu()
 
 	elif gl.prog_pos[0] == 'f':			# free mixing
@@ -54,6 +53,9 @@ while gl.prog_active:
 
 	elif gl.prog_pos[0] == 's':			# settings
 		ui.settings()
+	
+	elif gl.prog_pos[0] == 'q':			# quit / shutdown
+		ui.shutdown()
 
 	else:
 		text = ["ERROR", "invalid prog_pos: " + str(gl.prog_pos), "resetting to 'm'"]
@@ -66,7 +68,7 @@ while gl.prog_active:
 		prog_pos = 'm'
 
 
-	# debug
+	# debug infos
 	if gl.show_debug:
 		fps = str(gl.clock.get_fps())
 		debug_main_loop = ["FPS: " + fps[0:6], "prog_pos: " + gl.prog_pos]
@@ -76,9 +78,9 @@ while gl.prog_active:
 			pygame.draw.rect(gl.screen, (0,0,0), (0,gl.H - rect.height - h,rect.width,rect.height))
 			gl.screen.blit(textsur, (0,gl.H - rect.height - h))
 			h += rect.height + 3
-		gl.debug_text.clear()
+	gl.debug_text.clear()
 
-	pygame.display.flip()	# refresh window and show content
-	gl.clock.tick(gl.FPS)			# limit fps
+	pygame.display.flip()		# refresh window and show content
+	gl.clock.tick(gl.FPS)		# limit fps
 
 pygame.quit()

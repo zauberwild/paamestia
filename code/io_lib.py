b25ea2e0c7c883473b3c639e173143638a8a5077
zauberwild <zauberwild@users.noreply.github.com>
2021-01-21 19:28:07 +0100

    small changes and documentation

--- code/io_lib.py
+++ code/io_lib.py
@@ -45,7 +45,7 @@ def update_input():
 
 	# read current state
 	if gl.os_is_linux:				# for the raspberry pi
-		up_state	= not UP_BT.is_pressed
+		up_state	= not UP_BT.is_pressed			# read every input
 		down_state	= not DOWN_BT.is_pressed
 		left_state	= not LEFT_BT.is_pressed
 		right_state	= not RIGHT_BT.is_pressed
@@ -62,7 +62,7 @@ def update_input():
 				if(event.key == pygame.K_DELETE):	back_state 	= not back_state
 
 def readInput(input):
-	""" read signal from input. returns bool
+	""" returns input states as bool
 	input: chosen input [UP, DOWN, LEFT, RIGHT, NEXT, BACK] or any other gpio pin
 	"""
 	global UP, DOWN, LEFT, RIGHT, NEXT, BACK
@@ -74,9 +74,9 @@ def readInput(input):
 
 	is_pressed = False				# this variable will be set True when asked button is pressed. Otherwise it won't change
 
-	for key in keys:
-		if key[0] == input and key[2] == False:
-			is_pressed = key[1]
+	for key in keys:				# loops through key list
+		if key[0] == input and key[2] == False:		# if tested key equals input and requested key is pressed
+			is_pressed = key[1]						# set is_pressed = True
 	
 	return is_pressed
 
@@ -87,8 +87,8 @@ if not gl.os_is_linux:
 	ser = serial.Serial(PORT, BAUD)
 
 VALVES = [11, 0, 26, 19, 27, 22, 10]			# NOTE Valves: set corresponding pins here ([0] is the valve for water, then going from left to right)
-PUMP = 9	
-if gl.os_is_linux:									# 		 Pump: set gpio pin for pump here
+PUMP = 9										#		Pump:  set pin for pump here
+if gl.os_is_linux:
 	from gpiozero import LED
 	VALVES_OUT = [LED(VALVES[0]), LED(VALVES[1]), LED(VALVES[2]), LED(VALVES[3]), LED(VALVES[4]), LED(VALVES[5]), LED(VALVES[6])]
 	PUMP_OUT = LED(PUMP)

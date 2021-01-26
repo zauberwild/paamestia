3d9f7732016d1dedc81e3e82728ebae9f25fd1d9
zauberwild <zauberwild@users.noreply.github.com>
2021-01-26 18:00:55 +0100

    Update globals.py
    - reorganized variables
    - added options for drinks and output

--- code/globals.py
+++ code/globals.py
@@ -2,12 +2,34 @@
 this file stores all global variables and constants
 """
 
+""" imports """
 from pathlib import Path								# used to get the complete path of the working directory
-gen_path = str(Path(__file__).parent.absolute())		# get the complete path of the "code"-directory
 from os import path											# used to get the complete path of the working directory
-os_is_linux = not path.isfile(gen_path + "/src/.windows")		# looks for a ".windows" file, which only exists on my Windows-PC
 import pygame
+import pygame.freetype
 
+""" file paths """
+gen_path = str(Path(__file__).parent.absolute())		# get the complete path of the "code"-directory
+
+drink_file_path = gen_path + "/src/drinks"				# path of drinks file
+
+""" output variables """
+SIZE_GLASS = 250 					# size of glass in ml
+MIN_UNIT = 25						# smallest possible unit in ml
+TIME_PER_UNIT = 11					# time need to fill one unit in seconds
+N_UNITS = SIZE_GLASS / MIN_UNIT		# amount of units
+
+""" debug variables """
+os_is_linux = not path.isfile(gen_path + "/src/.windows")		# looks for a ".windows" file, which only exists on my Windows-PC
+
+show_debug = True		# show debugging information (fps,...)
+debug_text = []			# debug list with all parameters to show. append parameters to this list every loop
+
+pygame.freetype.init()
+debug_font = pygame.freetype.Font(gen_path + "/src/fonts/CamingoCode-Regular.ttf", 24)		# debug font
+debug_font_big = pygame.freetype.Font(gen_path + "/src/fonts/CamingoCode-Regular.ttf", 48)		# debug font, but bigger
+
+""" setting window up """
 prog_active = True		# set to False to end program
 
 FPS = 24				# frames per second
@@ -15,14 +37,6 @@ W, H = 800, 600			# width and height of the window
 
 prog_pos = 'i'			# saves current position in program flow
 
-show_debug = True		# show debugging information (fps,...)
-debug_text = []			# debug list with all parameters to show. append parameters to this list every loop
-
-debug_font = pygame.freetype.Font(gen_path + "/src/fonts/CamingoCode-Regular.ttf", 24)		# debug font
-debug_font_big = pygame.freetype.Font(gen_path + "/src/fonts/CamingoCode-Regular.ttf", 48)		# debug font, but bigger
-
-# setting the window up
-import pygame
 screen = None
 if os_is_linux:
 	screen = pygame.display.set_mode((W,H), pygame.FULLSCREEN)

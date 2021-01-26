0f8e358f72e8464ca040a558118f639ba47763af
zauberwild <zauberwild@users.noreply.github.com>
2021-01-26 18:00:11 +0100

    Update drinks_lib.py
    started implentation drinks & recipes

--- code/drinks_lib.py
+++ code/drinks_lib.py
@@ -2,10 +2,15 @@
 contains the recipe-class and drink-class
 """
 
-class recipe:
+import globals as gl
+import io_lib as io
+import time
+
+""" ### CLASSES ### """
+
+class Recipe:
 	"""
 	stores and handles the available recipes
-	TODO rework / optimisation
 	"""
 
 	def __init__(self, path):
@@ -13,33 +18,73 @@ class recipe:
 		self.description = ""
 		self.drinks = []
 		self.vol = []
-		self.loadRecipe()
+		self.loadRecipe(path)
 
-	def loadRecipe(self, new_path = ""):
+	def loadRecipe(self, new_path):
 		""" load recipe
 		- new_path: use to set a new recipe up
 		"""
-		if new_path == "":
-			new_path = self.path
+		self.path = new_path
 
-class drink:
+class Drink:
 	"""
 	this class handles the spots, remembers the plugged drinks
 	and controls the output
 	"""
 
-	def __init__(self,spot):
+	def __init__(self,spot, drink=""):
+		""" Drink class
+		- spot: number of spot [0: cleaning water, 1-6: drink spots]
+		- drink="": drink placed in this spot (opt.)
+		"""
 		self.spot = spot
-		self.drink
+		self.drink = ""
 
-	def assignDrink(self,drink):
+		self.filling = False
+		self.t_start = 0
+		self.vol = 0
+
+	def setDrink(self,drink):
 		""" set a new drink
 		- drink: the new drink
 		"""
-		pass
+		self.drink = drink
 
 	def fill(self,vol):
 		"""	start to fill the glass
 		- vol: the needed volume in ml
 		"""
+		self.filling = True
+		self.t_start = time.time()
+		self.vol = vol
+	
+	def update(self):
 		pass
+
+
+""" ### FUNCTIONS AND VARIABLES ### """
+
+""" drinks list """
+drinks_list = []
+def refresh_drinks_list():
+	""" read "drinks" file and save in list """
+	global drinks_list
+
+	file = open(gl.drink_file_path, 'r')		# read file
+	drinks_list = file.readlines()
+	file.close()
+	file = open(gl.drink_file_path, 'r')		# read file
+	drinks_list = file.readlines()
+	file.close()
+
+	for idx, i in enumerate(drinks_list):		# remove trailing newline characters
+		drinks_list[idx] = drinks_list[idx].removesuffix("\n")
+
+refresh_drinks_list()
+
+""" spots list """
+spot = []
+for i in range(7):
+	spot.append(Drink(i))
+
+""" recipes """

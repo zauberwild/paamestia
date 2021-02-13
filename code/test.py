7a3d9cff8e2fcdab7fb2f5b5655c7631aadb046a
zauberwild <zauberwild@users.noreply.github.com>
2021-02-13 16:21:34 +0100

    Update test.py
    testing purposes (outdated)

--- code/test.py
+++ code/test.py
@@ -7,4 +7,11 @@ import pygame
 import pygame.freetype
 import globals as gl
 
-print(drinks.drinks_list)
+drinks.spot[5].drink = "Alter Janx-Geist"
+drinks.spot[4].drink = "arkturanischer Mega-Gin"
+drinks.spot[6].drink = "fallianisches Sumpfgas"
+drinks.spot[2].drink = "qualaktinischen Hyperminz-Extrakt"
+drinks.spot[1].drink = "Zamphuor"
+drinks.spot[3].drink = "KiBa"
+
+pangal = drinks.Recipe(gl.gen_path + "/src/recipes/Pangalaktischer_Donnergurgler")
